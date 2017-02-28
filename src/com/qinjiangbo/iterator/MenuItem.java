3410fc2e89cbdaed62c2a73a72f1cb71ef4d6a9f
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-02-28 21:13:39 +0800

    add DinerMenu and PancakeHouseMenu class for Iterator Design Patterns

--- src/com/qinjiangbo/iterator/MenuItem.java
+++ src/com/qinjiangbo/iterator/MenuItem.java
@@ -11,10 +11,8 @@ public class MenuItem {
     private boolean vegetarian;
     private double price;
 
-    public MenuItem(String name,
-                    String description,
-                    boolean vegetarian,
-                    double price) {
+    public MenuItem(String name, String description,
+                    boolean vegetarian, double price) {
         this.name = name;
         this.description = description;
         this.vegetarian = vegetarian;
--- src/com/qinjiangbo/iterator/PancakeHouseMenu.java
+++ src/com/qinjiangbo/iterator/PancakeHouseMenu.java
@@ -1,8 +1,28 @@
 package com.qinjiangbo.iterator;
 
+import java.util.ArrayList;
+
 /**
  * @date: 28/02/2017 9:03 PM
  * @author: qinjiangbo@github.io
  */
 public class PancakeHouseMenu {
+
+    ArrayList menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList();
+
+        addItem("K&B's Pancake Breakfast",
+                "Pancakes with scrambled eggs, and toast",
+                true,
+                2.99);
+
+    }
+
+    public void addItem(String name, String description,
+                        boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.add(menuItem);
+    }
 }
