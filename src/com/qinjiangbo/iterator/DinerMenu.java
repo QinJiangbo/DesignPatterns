067406d6fefd22a94b2a8c829ff6d19ec9530019
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-02-28 21:25:22 +0800

    add DinerMenu and PancakeHouseMenu class for Iterator Design Patterns

--- src/com/qinjiangbo/iterator/DinerMenu.java
+++ src/com/qinjiangbo/iterator/DinerMenu.java
@@ -13,11 +13,34 @@ public class DinerMenu {
     public DinerMenu() {
         menuItems = new MenuItem[MAX_SIZE];
 
+        addItem("Vegetarian BLT",
+                "(Fakin) Bacon with lettuce & tomato on the whole wheat",
+                true,
+                2.99);
+
+        addItem("BLT",
+                "Bacon with lettuce & tomato on the whole wheat",
+                false,
+                2.99);
+
+        addItem("Soup of the day",
+                "Soup of the day, with a side of potato salad",
+                false,
+                3.29);
+
+        addItem("Hotdog",
+                "A hot dog",
+                false,
+                3.05);
     }
 
     public void addItem(String name, String description,
                         boolean vegetarian, double price) {
         MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
-        menuItems[numberOfItems++] = menuItem;
+        if (numberOfItems >= MAX_SIZE) {
+            System.err.println("Sorry, menu is full! Can't add items!");
+        } else {
+            menuItems[numberOfItems++] = menuItem;
+        }
     }
 }
