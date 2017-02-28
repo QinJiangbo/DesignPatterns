05f0d7b5bd60380f410750e30a170269f4e12122
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-02-28 21:17:35 +0800

    add DinerMenu and PancakeHouseMenu class for Iterator Design Patterns

--- src/com/qinjiangbo/iterator/PancakeHouseMenu.java
+++ src/com/qinjiangbo/iterator/PancakeHouseMenu.java
@@ -18,11 +18,30 @@ public class PancakeHouseMenu {
                 true,
                 2.99);
 
+        addItem("Regular Pancake Breakfast",
+                "Pancakes with fried eggs, sausage",
+                false,
+                2.99);
+
+        addItem("Blueberry Pancake",
+                "Pancakes made with fresh blueberries",
+                true,
+                3.49);
+
+        addItem("Waffles",
+                "Waffles, with your choices of blueberries or strawberries",
+                true,
+                3.59);
+
     }
 
     public void addItem(String name, String description,
                         boolean vegetarian, double price) {
         MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
         menuItems.add(menuItem);
+    }
+
+    public ArrayList getMenuItems() {
+        return menuItems;
     }
 }
