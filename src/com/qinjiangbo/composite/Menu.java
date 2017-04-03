04c05239921e68760d07ea3db59ea1d906cabee9
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-04-03 23:59:26 +0800

    add Waitress class

--- src/com/qinjiangbo/composite/Menu.java
+++ src/com/qinjiangbo/composite/Menu.java
@@ -13,6 +13,11 @@ public class Menu extends MenuComponent {
     private String name;
     private String description;
 
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
     @Override
     public void add(MenuComponent menuComponent) {
         menuComponents.add(menuComponent);
---
+++ src/com/qinjiangbo/composite/Test.java
@@ -0,0 +1,35 @@
+package com.qinjiangbo.composite;
+
+/**
+ * @date: 03/04/2017 11:50 PM
+ * @author: qinjiangbo@github.io
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        MenuComponent pancakeMenu =
+                new Menu("PANCAKE HOUSE MENU", "Breakfast");
+        MenuComponent dinerMenu =
+                new Menu("DINER MENU", "Lunch");
+        MenuComponent cafeMenu =
+                new Menu("CAFE MENU", "Diner");
+        MenuComponent dessertMenu =
+                new Menu("DESSERT MENU", "Dessert");
+
+        MenuComponent allMenus = new Menu("ALL MENUS", "all menu");
+        allMenus.add(pancakeMenu);
+        allMenus.add(dinerMenu);
+        allMenus.add(cafeMenu);
+        allMenus.add(dessertMenu);
+
+        dinerMenu.add(new MenuItem("Gong Bao Ji Ding", "An old food",
+                true, 20.32));
+
+        cafeMenu.add(new MenuItem("Starbucks", "coffee",
+                false, 16.00));
+
+        Waitress waitress = new Waitress(allMenus);
+        waitress.printMenu();
+
+    }
+}
