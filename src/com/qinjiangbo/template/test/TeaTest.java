4c078a4647d19dae9bc65006e1b3fd223d979b7c
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-12-25 13:17:42 +0800

    修改模板方法模式实例代码

--- src/com/qinjiangbo/template/test/TeaTest.java
+++ src/com/qinjiangbo/template/test/TeaTest.java
@@ -3,8 +3,6 @@ package com.qinjiangbo.template.test;
 import com.qinjiangbo.template.methods.Coffee;
 import com.qinjiangbo.template.methods.Tea;
 
-import java.util.Arrays;
-
 /**
  * Created by IntelliJ IDEA.
  * Date: 10/25/15 10:26 PM
